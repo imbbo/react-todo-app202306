2c0816e99e9a28444a922340ef9deb5200d38ce9
KJH <0311wlsgus@gmail.com>
2023-06-14 12:12:41 +0900

    추가버튼 및 할일 목록 렌더링

--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import TodoItem from './TodoItem';
 
-
 import'./scss/TodoMain.scss';
-const TodoMain = () => {
-
+const TodoMain = ({ todoList }) => {
+  
   return (
     <ul className='todo-list'>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {
+          todoList.map(todo => <TodoItem item={todo} />)
+        }
     </ul>
     );
 }
